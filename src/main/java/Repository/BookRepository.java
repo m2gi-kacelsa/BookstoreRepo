2b5fe6ada6325196e6cefda6cbe5b3dc82c3c826
sabrinaKacel <sabrina.kacel@eadn.dz>
2019-08-01 11:52:52 +0100

    Module 5 Transaction

---
+++ src/main/java/Repository/BookRepository.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repository;
+
+import com.bookstoreBack.model.Book;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+/**
+ *
+ * @author DELL_PC
+ */
+
+@Transactional(SUPPORTS)
+public class BookRepository {
+    
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager em;
+    
+    public Book find(Long id){
+        return em.find(Book.class, id);
+    }
+    
+    public List<Book> findAll() {
+        TypedQuery<Book> query = em.createQuery("SELECT b from Book b ORDER BY b.title DESC", Book.class);
+        return query.getResultList();
+    }
+    
+    public Long countAll(){
+        TypedQuery<Long> query = em.createQuery("SELECT COUNT(b) from Book b", Long.class);
+        return query.getSingleResult();
+    }
+    
+    @Transactional(REQUIRED)
+    public Book create(Book book){
+        em.persist(book);
+        return book;
+    }
+    
+    @Transactional(REQUIRED)
+    public void delete(Long id){
+        em.remove(em.getReference(Book.class, id));
+    }
+    
+}
